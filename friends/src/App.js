e442086bb487d9e7a25d8d03a86227116fae8a9e
TheRealMPC <smkscar25@hotmail.com>
2017-08-15 03:34:32 -0500

    React Friends CRA complete (although the images are all 404 now)

---
+++ friends/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import FriendsList from './components/FriendsList';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>The <strong>facebook</strong> Friend Machine</h1>
+        <div className="friends"></div>
+        <FriendsList/>
+      </div>
+    );
+  }
+}
+
+export default App;
